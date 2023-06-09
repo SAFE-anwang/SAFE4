468b3fa8f861cb050cee0e0390ba712be01833b6
lemengbin <lemengbin@163.com>
2023-06-09 15:59:03 +0800

    update types

--- core/types/masternode.go
+++ core/types/masternode.go
@@ -10,31 +10,31 @@ import (
 )
 
 type MasterNodeMemberInfo struct {
-	LockID *big.Int
-	Addr   common.Address
-	Amount *big.Int
-	Height *big.Int
+	LockID *big.Int         `json:"lockID"    gencodec:"required"`
+	Addr   common.Address   `json:"addr"      gencodec:"required"`
+	Amount *big.Int         `json:"amount"    gencodec:"required"`
+	Height *big.Int         `json:"height"    gencodec:"required"`
 }
 
 type MasterNodeIncentivePlan struct {
-	Creator *big.Int
-	Partner *big.Int
-	Voter   *big.Int
+	Creator *big.Int        `json:"creator"   gencodec:"required"`
+	Partner *big.Int        `json:"partner"   gencodec:"required"`
+	Voter   *big.Int        `json:"voter"     gencodec:"required"`
 }
 
 type MasterNodeInfo struct {
-	Id            *big.Int
-	Addr          common.Address
-	Creator       common.Address
-	Amount        *big.Int
-	Enode         string
-	Ip            string
-	Description   string
-	State         *big.Int
-	Founders      []MasterNodeMemberInfo
-	IncentivePlan MasterNodeIncentivePlan
-	CreateHeight  *big.Int
-	UpdateHeight  *big.Int
+	Id            *big.Int                  `json:"id"            gencodec:"required"`
+	Addr          common.Address            `json:"addr"          gencodec:"required"`
+	Creator       common.Address            `json:"creator"       gencodec:"required"`
+	Amount        *big.Int                  `json:"amount"        gencodec:"required"`
+	Enode         string                    `json:"enode"         gencodec:"required"`
+	Ip            string                    `json:"ip"            gencodec:"required"`
+	Description   string                    `json:"description,omitempty"`
+	State               *big.Int            `json:"state"         gencodec:"required"`
+	Founders      []MasterNodeMemberInfo    `json:"founders"      gencodec:"required"`
+	IncentivePlan MasterNodeIncentivePlan   `json:"incentivePlan" gencodec:"required"`
+	CreateHeight  *big.Int                  `json:"createHeight,omitempty"`
+	UpdateHeight  *big.Int                  `json:"updateHeight,omitempty"`
 }
 
 // MasterNodePing is an masternode ping.
--- core/types/proposal.go
+++ core/types/proposal.go
@@ -6,18 +6,18 @@ import (
 )
 
 type ProposalInfo struct {
-	Id           *big.Int
-	Creator      common.Address
-	Title        string
-	PayAmount    *big.Int
-	PayTimes     *big.Int
-	StartPayTime *big.Int
-	EndPayTime   *big.Int
-	Description  string
-	Detail       string
-	Voters       []common.Address
-	VoteResults  []*big.Int
-	State        *big.Int
-	CreateHeight *big.Int
-	UpdateHeight *big.Int
+	Id           *big.Int           `json:"id"              gencodec:"required"`
+	Creator      common.Address     `json:"creator"         gencodec:"required"`
+	Title        string             `json:"title"           gencodec:"required"`
+	PayAmount    *big.Int           `json:"payAmount"       gencodec:"required"`
+	PayTimes     *big.Int           `json:"payTimes"        gencodec:"required"`
+	StartPayTime *big.Int           `json:"startPayTime"    gencodec:"required"`
+	EndPayTime   *big.Int           `json:"endPayTime"      gencodec:"required"`
+	Description  string             `json:"description"     gencodec:"required"`
+	Detail       string             `json:"detail"          gencodec:"required"`
+	Voters       []common.Address   `json:"voters,omitempty"`
+	VoteResults  []*big.Int         `json:"voteResults,omitempty"`
+	State        *big.Int           `json:"state"           gencodec:"required"`
+	CreateHeight *big.Int           `json:"createHeight,omitempty"`
+	UpdateHeight *big.Int           `json:"UpdateHeight,omitempty"`
 }

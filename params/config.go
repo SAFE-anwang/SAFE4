768e365102541294c49c67507ceb1746609db596
shuqijkx <shuqijkx@163.com>
2023-06-12 14:52:36 +0800

    The modification of the genesis.json file resulted in a change in SafeGenesisHash

--- params/config.go
+++ params/config.go
@@ -33,7 +33,7 @@ var (
 	RinkebyGenesisHash = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
 	KilnGenesisHash    = common.HexToHash("0x51c7fe41be669f69c45c33a56982cbde405313342d9e2b00d7c91a7b284dd4f8")
-	SafeGenesisHash    = common.HexToHash("0x1236d1ea1fabb06f8fc0df3b70afc1ceb00fcd74dca4d03ec6c254ec985a5816")
+	SafeGenesisHash    = common.HexToHash("0x4c89bffd4362eab34d6d0eeae450d76176497b5b6768ef410f2f962b9cce131d")
 )
 
 // TrustedCheckpoints associates each known checkpoint with the genesis hash of

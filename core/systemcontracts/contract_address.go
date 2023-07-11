7fa8ad68d2f1bfebfefe25edb80474a1a2e6b746
lemengbin <lemengbin@163.com>
2023-07-11 15:48:31 +0800

    update system-contracts' ABI

--- core/systemcontracts/contract_address.go
+++ core/systemcontracts/contract_address.go
@@ -2,6 +2,8 @@ package systemcontracts
 
 import "github.com/ethereum/go-ethereum/common"
 
+var SystemContractAddr = common.HexToAddress("0x0000000000000000000000000000000000000992")
+
 var PropertyContractAddr = common.HexToAddress("0x0000000000000000000000000000000000001002")
 
 var AccountManagerContractAddr = common.HexToAddress("0x0000000000000000000000000000000000001012")
@@ -19,5 +21,7 @@ var SuperNodeStateContractAddr = common.HexToAddress("0x000000000000000000000000
 var ProposalContractAddr = common.HexToAddress("0x0000000000000000000000000000000000001072")
 
 var SystemRewardContractAddr = common.HexToAddress("0x0000000000000000000000000000000000001082")
+
+var Safe3ContractAddr = common.HexToAddress("0x0000000000000000000000000000000000001092")
 
 var MultiCallContractAddr = common.HexToAddress("0x0000000000000000000000000000000000001102")

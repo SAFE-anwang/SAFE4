746cd4eec9f530df176f25ea261f7a02ce0e0682
lemengbin <lemengbin@163.com>
2023-09-15 17:52:10 +0800

    fix bug

--- core/state_transition.go
+++ core/state_transition.go
@@ -199,7 +199,7 @@ func (st *StateTransition) buyGas() error {
 		balanceCheck = balanceCheck.Mul(balanceCheck, st.gasFeeCap)
 		balanceCheck.Add(balanceCheck, st.value)
 	}
-	if *st.msg.To() != systemcontracts.SystemRewardContractAddr {
+	if st.msg.To() == nil || *st.msg.To() != systemcontracts.SystemRewardContractAddr {
 		if have, want := st.state.GetBalance(st.msg.From()), balanceCheck; have.Cmp(want) < 0 {
 			return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), have, want)
 		}
@@ -316,7 +316,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	st.gas -= gas
 
 	// Check clause 6
-	if *msg.To() != systemcontracts.SystemRewardContractAddr && msg.Value().Sign() > 0 && !st.evm.Context.CanTransfer(st.state, msg.From(), *msg.To(), msg.Value()) {
+	if (msg.To() == nil || *msg.To() != systemcontracts.SystemRewardContractAddr) && msg.Value().Sign() > 0 && !st.evm.Context.CanTransfer(st.state, msg.From(), *msg.To(), msg.Value()) {
 		return nil, fmt.Errorf("%w: address %v", ErrInsufficientFundsForTransfer, msg.From().Hex())
 	}
 

a55ea0d9b69bb5e9330a31c8fd6612da9642f623
lemengbin <lemengbin@163.com>
2023-11-03 18:54:01 +0800

    modify type

--- core/types/proposal.go
+++ core/types/proposal.go
@@ -6,17 +6,17 @@ import (
 )
 
 type ProposalInfo struct {
-	Id           *big.Int           `json:"id"              gencodec:"required"`
-	Creator      common.Address     `json:"creator"         gencodec:"required"`
-	Title        string             `json:"title"           gencodec:"required"`
-	PayAmount    *big.Int           `json:"payAmount"       gencodec:"required"`
-	PayTimes     *big.Int           `json:"payTimes"        gencodec:"required"`
-	StartPayTime *big.Int           `json:"startPayTime"    gencodec:"required"`
-	EndPayTime   *big.Int           `json:"endPayTime"      gencodec:"required"`
-	Description  string             `json:"description"     gencodec:"required"`
-	Voters       []common.Address   `json:"voters,omitempty"`
-	VoteResults  []*big.Int         `json:"voteResults,omitempty"`
-	State        *big.Int           `json:"state"           gencodec:"required"`
-	CreateHeight *big.Int           `json:"createHeight,omitempty"`
-	UpdateHeight *big.Int           `json:"UpdateHeight,omitempty"`
+	Id           *big.Int           `json:"id"                      gencodec:"required"`
+	Creator      common.Address     `json:"creator"                 gencodec:"required"`
+	Title        string             `json:"title"                   gencodec:"required"`
+	PayAmount    *big.Int           `json:"payAmount"               gencodec:"required"`
+	PayTimes     *big.Int           `json:"payTimes"                gencodec:"required"`
+	StartPayTime *big.Int           `json:"startPayTime"            gencodec:"required"`
+	EndPayTime   *big.Int           `json:"endPayTime"              gencodec:"required"`
+	Description  string             `json:"description"             gencodec:"required"`
+	Voters       []common.Address   `json:"voters"                  gencodec:"required"`
+	VoteResults  []*big.Int         `json:"voteResults"             gencodec:"required"`
+	State        *big.Int           `json:"state"                   gencodec:"required"`
+	CreateHeight *big.Int           `json:"createHeight"            gencodec:"required"`
+	UpdateHeight *big.Int           `json:"UpdateHeight"            gencodec:"required"`
 }

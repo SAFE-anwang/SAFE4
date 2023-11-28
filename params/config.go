c2e1c5b3dfd1fe4455c1e20f95f799fac9906e2b
lemengbin <lemengbin@163.com>
2023-11-28 17:02:06 +0800

    update genesis block

--- params/config.go
+++ params/config.go
@@ -33,7 +33,7 @@ var (
 	RinkebyGenesisHash = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
 	KilnGenesisHash    = common.HexToHash("0x51c7fe41be669f69c45c33a56982cbde405313342d9e2b00d7c91a7b284dd4f8")
-	SafeGenesisHash    = common.HexToHash("0x5a906f35d3dc072406ea6371759664c5cf16b237b8ad8b4ea41e7cb04488a57c")
+	SafeGenesisHash    = common.HexToHash("0xdcd4c0a7c4fde52a000c11601143f7db0b2268a7bc22dc527071cd02811602ea")
 )
 
 // TrustedCheckpoints associates each known checkpoint with the genesis hash of

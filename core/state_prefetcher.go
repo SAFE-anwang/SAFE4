13873b2498bb3a59fac5974d3b9dd1fe610a3c7d
lemengbin <lemengbin@163.com>
2024-10-11 15:11:04 +0800

    pre-cache all txs, but nedd to check balance when snapshot is enable

--- core/state_prefetcher.go
+++ core/state_prefetcher.go
@@ -58,24 +58,22 @@ func (p *statePrefetcher) Prefetch(block *types.Block, statedb *state.StateDB, c
 	// Iterate over and process the individual transactions
 	byzantium := p.config.IsByzantium(block.Number())
 	for i, tx := range block.Transactions() {
-		if i != block.Transactions().Len() - 1 {
-			// If block precaching was interrupted, abort
-			if interrupt != nil && atomic.LoadUint32(interrupt) == 1 {
-				return
-			}
-			// Convert the transaction into an executable message and pre-cache its sender
-			msg, err := tx.AsMessage(signer, header.BaseFee)
-			if err != nil {
-				return // Also invalid block, bail out
-			}
-			statedb.Prepare(tx.Hash(), i)
-			if err := precacheTransaction(msg, p.config, gaspool, statedb, header, evm); err != nil {
-				return // Ugh, something went horribly wrong, bail out
-			}
-			// If we're pre-byzantium, pre-load trie nodes for the intermediate root
-			if !byzantium {
-				statedb.IntermediateRoot(true)
-			}
+		// If block precaching was interrupted, abort
+		if interrupt != nil && atomic.LoadUint32(interrupt) == 1 {
+			return
+		}
+		// Convert the transaction into an executable message and pre-cache its sender
+		msg, err := tx.AsMessage(signer, header.BaseFee)
+		if err != nil {
+			return // Also invalid block, bail out
+		}
+		statedb.Prepare(tx.Hash(), i)
+		if err := precacheTransaction(msg, p.config, gaspool, statedb, header, evm); err != nil {
+			return // Ugh, something went horribly wrong, bail out
+		}
+		// If we're pre-byzantium, pre-load trie nodes for the intermediate root
+		if !byzantium {
+			statedb.IntermediateRoot(true)
 		}
 	}
 	// If were post-byzantium, pre-load trie nodes for the final root hash
